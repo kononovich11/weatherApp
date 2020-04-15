b42046256f0e3859dc493b6de0a1634f6cb262c5
kononovich11 <kononovich1703@gmail.com>
2020-04-15 13:56:44 +0300

    add card with title

--- script.js
+++ script.js
@@ -10,24 +10,41 @@ const inputSearch = document.querySelector('.autocompleteSearch');
 form.addEventListener('submit', (e) => {
     e.preventDefault();
     const city = getCityName();
-    //console.log(city);
-    getDataFromApi(city);
-  })
+     getDataFromApi(city);
+  });
 
 function getCityName() {
     const inputSearchValue = inputSearch.value;
     return inputSearchValue;
 }
 
-async function getDataFromApi(city) {
-    return fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=8404e484fd35ab1089ddcd4ce507e732`)
+function createElementsOfCard(data) {
+    const card = document.querySelector('.card');
+    const cardTitle = document.createElement('h3');
+    cardTitle.textContent = data.name;
+    card.appendChild(cardTitle);
+    console.log(card);
+}
+
+ async function getDataFromApi(city) {
+    let data = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=8404e484fd35ab1089ddcd4ce507e732`)
     .then(response => {
         return response.json();
     })
     .then(dataObj => {
-        console.log(dataObj);
+       return dataObj;
     })
     .catch(err => {
         console.log(err);
     });
-}  
+    
+    getObjAllData(data);
+} 
+
+
+function getObjAllData(data) {
+    createElementsOfCard(data)
+
+}
+
+
